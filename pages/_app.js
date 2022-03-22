bac610cc5d31cd1257a75c7458545474489d8a69
Amanchik <bakytuulu00@bk.ru>
2022-03-22 22:23:05 +0300

    onlytwo

--- pages/_app.js
+++ pages/_app.js
@@ -9,24 +9,24 @@ function MyApp({ Component, pageProps }) {
           type: 'website',
           title:'Home',
           site_name:'new hone site',
-          url: 'https://ninja-list.vercel.app/',
+          url: 'https://ninja-list-amanbakytuulu.vercel.app/',
           site_name: 'SiteName',
           images: [
             {
-              url: 'https://ninja-list.vercel.app/react.jpg',
+              url: 'https://ninja-list-amanbakytuulu.vercel.app/react.jpg',
               width: 800,
               height: 600,
               alt: 'Og Image Alt',
               type: 'image/jpeg',
             },
             {
-              url: 'https://ninja-list.vercel.app/react.jpg',
+              url: 'https://ninja-list-amanbakytuulu.vercel.app/react.jpg',
               width: 1600,
               height: 900,
               alt: 'Og Image Alt Second',
               type: 'image/jpeg',
             },
-            { url: 'https://ninja-list.vercel.app/react.jpg' },
+            { url: 'https://ninja-list-amanbakytuulu.vercel.app/react.jpg' },
           ]
         }} />
       <Component {...pageProps} />
