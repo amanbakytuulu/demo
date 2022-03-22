442171228a77ea661e37cf9536859ff15214e359
Amanchik <bakytuulu00@bk.ru>
2022-03-22 22:01:14 +0300

    changed

--- pages/_app.js
+++ pages/_app.js
@@ -8,25 +8,25 @@ function MyApp({ Component, pageProps }) {
         openGraph={{
           type: 'website',
           title:'Home',
-          site_name:'prob.com.net',
-          url: 'https://www.url.ie/',
+          site_name:'new hone site',
+          url: 'https://demo-9rxmiim7v-amanbakytuulu.vercel.app/',
           site_name: 'SiteName',
           images: [
             {
-              url: 'https://demo/react.jpg',
+              url: 'https://https://demo-9rxmiim7v-amanbakytuulu.vercel.app//react.jpg',
               width: 800,
               height: 600,
               alt: 'Og Image Alt',
               type: 'image/jpeg',
             },
             {
-              url: 'https://demo/react.jpg',
+              url: 'https://https://demo-9rxmiim7v-amanbakytuulu.vercel.app//react.jpg',
               width: 1600,
               height: 900,
               alt: 'Og Image Alt Second',
               type: 'image/jpeg',
             },
-            { url: 'https://demo/react.jpg' },
+            { url: 'https://https://demo-9rxmiim7v-amanbakytuulu.vercel.app//react.jpg' },
           ]
         }} />
       <Component {...pageProps} />
