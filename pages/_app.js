094ae124c287a34041617d0fad7b4e71a8c6b77f
Amanchik <bakytuulu00@bk.ru>
2022-03-22 21:53:32 +0300

    new

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
+import { DefaultSeo } from 'next-seo'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          title:'Home',
+          site_name:'prob.com.net',
+          url: 'https://www.url.ie/',
+          site_name: 'SiteName',
+          images: [
+            {
+              url: 'https://demo.ie/react.jpg',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt',
+              type: 'image/jpeg',
+            },
+            {
+              url: 'https://demo.ie/react.jpg',
+              width: 1600,
+              height: 900,
+              alt: 'Og Image Alt Second',
+              type: 'image/jpeg',
+            },
+            { url: 'https://demo.ie/react.jpg' },
+          ]
+        }} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
