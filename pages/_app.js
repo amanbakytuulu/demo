798954953254b06f3b8bb1e49da3d5e5f79ce135
aman <bakytuulu@bk.ru>
2022-03-23 12:59:19 +0600

    1111

--- pages/_app.js
+++ pages/_app.js
@@ -9,24 +9,24 @@ function MyApp({ Component, pageProps }) {
           type: 'website',
           title:'Home',
           site_name:'demo-list',
-          url: 'https://demo-list-amanbakytuulu.vercel.app/',
+          url: 'https://example-list-amanbakytuulu.vercel.app/',
           description:'example for opengraph',
           images: [
             {
-              url: 'https://demo-list-amanbakytuulu.vercel.app/logo.jpg',
+              url: 'https://example-list-amanbakytuulu.vercel.app/logo.jpg',
               width: 800,
               height: 600,
               alt: 'Og Image Alt',
               type: 'image/jpeg',
             },
             {
-              url: 'https://demo-list-amanbakytuulu.vercel.app/logo.jpg',
+              url: 'https://example-list-amanbakytuulu.vercel.app/logo.jpg',
               width: 1600,
               height: 900,
               alt: 'Og Image Alt Second',
               type: 'image/jpeg',
             },
-            { url: 'https://demo-list-amanbakytuulu.vercel.app/logo.jpg' },
+            { url: 'https://example-list-amanbakytuulu.vercel.app/logo.jpg' },
           ]
         }} />
       <Component {...pageProps} />
