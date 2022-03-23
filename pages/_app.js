6975f3e4fc2168caaaf7ec150fb23fe931c53c12
aman <bakytuulu@bk.ru>
2022-03-23 12:33:23 +0600

    onlychanged

--- pages/_app.js
+++ pages/_app.js
@@ -9,24 +9,24 @@ function MyApp({ Component, pageProps }) {
           type: 'website',
           title:'Home',
           site_name:'new hone site',
-          url: 'https://ninja-list-amanbakytuulu.vercel.app/',
+          url: 'https://demo-list-amanbakytuulu.vercel.app/',
           site_name: 'SiteName',
           images: [
             {
-              url: 'https://ninja-list-amanbakytuulu.vercel.app/logo.png',
+              url: 'https://demo-list-amanbakytuulu.vercel.app/logo.png',
               width: 800,
               height: 600,
               alt: 'Og Image Alt',
               type: 'image/jpeg',
             },
             {
-              url: 'https://ninja-list-amanbakytuulu.vercel.app/logo.png',
+              url: 'https://demo-list-amanbakytuulu.vercel.app/logo.png',
               width: 1600,
               height: 900,
               alt: 'Og Image Alt Second',
               type: 'image/jpeg',
             },
-            { url: 'https://ninja-list-amanbakytuulu.vercel.app/logo.png' },
+            { url: 'https://demo-list-amanbakytuulu.vercel.app/logo.png' },
           ]
         }} />
       <Component {...pageProps} />
