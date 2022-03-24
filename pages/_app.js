e8fb961b4172a93e79506af675d487ec6453d9f2
aman <bakytuulu@bk.ru>
2022-03-24 10:43:00 +0600

    newChange

--- pages/_app.js
+++ pages/_app.js
@@ -13,20 +13,20 @@ function MyApp({ Component, pageProps }) {
           description:'example for opengraph',
           images: [
             {
-              url: 'https://demo-list-amanbakytuulu.vercel.app/logo.jpg',
+              url: 'https://demo-list-amanbakytuulu.vercel.app/logo.png',
               width: 800,
               height: 600,
               alt: 'Og Image Alt',
               type: 'image/jpeg',
             },
             {
-              url: 'https://demo-list-amanbakytuulu.vercel.app/logo.jpg',
+              url: 'https://demo-list-amanbakytuulu.vercel.app/logo.png',
               width: 1600,
               height: 900,
               alt: 'Og Image Alt Second',
               type: 'image/jpeg',
             },
-            { url: 'https://demo-list-amanbakytuulu.vercel.app/logo.jpg' },
+            { url: 'https://demo-list-amanbakytuulu.vercel.app/logo.png' },
           ]
         }} />
       <Component {...pageProps} />
